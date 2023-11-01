454854e42a35bb484cce71be54d6a63c6fd6e255
michalmlk <michal.melka1@gmail.com>
2023-11-01 23:48:12 +0100

    basic setup and check data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { DataService } from './DataService.ts';
+import { DocumentData } from 'firebase/firestore';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [testData, setTestData] = useState<DocumentData>();
+    const service = new DataService();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const getData = async () => {
+            const _data = await service.getAllMeds();
+            setTestData(_data);
+        };
+        getData();
+    }, []);
+
+    return (
+        <>
+            <div>Test</div>
+            <ul>{testData && testData.map((d) => <li key={d.id}>{d.name}</li>)}</ul>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/DataService.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { DocumentData, collection, getDocs, getFirestore } from 'firebase/firestore';
+import { firebaseConfig } from '../config';
+
+export class DataService {
+    private app = initializeApp(firebaseConfig);
+    private db = getFirestore(this.app);
+
+    async getAllMeds() {
+        const data: Array<DocumentData> = [];
+        const querySnapshot = await getDocs(collection(this.db, 'ownedMeds'));
+        querySnapshot.forEach((q) => {
+            const item = q.data();
+            data.push(item);
+        });
+        return data;
+    }
+}
